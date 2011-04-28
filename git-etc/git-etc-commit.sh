4306faab024d6a75d2d2f9d790c38b91ff29affc
Niko Ehrenfeuchter <niko.ehrenfeuchter@zbsa.de>
2011-04-28 16:47:53 +0200

    make output work with thunderbird diff-addon
     * explicitly require git base-dir as first parameter
     * rearrange the output to be usable as mail-body that works with
       Thunderbird's "Colored Diffs" Add-on [1] while still remaining
       human-readable at the same time
    
    [1] https://addons.mozilla.org/en-US/thunderbird/addon/colored-diffs/

--- git-etc/git-etc-commit.sh
+++ git-etc/git-etc-commit.sh
@@ -3,7 +3,25 @@
 
 # set -x
 
-cd /etc
+_exit_usage() {
+    echo "USAGE: $0 /full/path/to/git/repo/"
+    echo
+    exit 100
+}
+
+if [ -z "$1" ] ; then
+    _exit_usage
+fi
+
+set -e
+cd "$1"
+set +e
+
+if ! [ -d ".git" ] ; then
+    echo "ERROR: '$1' is no git repository!"
+    echo
+    _exit_usage
+fi
 
 # 'add' is required to track new files
 git add .
@@ -11,24 +29,24 @@ git add .
 # only call 'commit' on real changes, otherwise git exits
 # with status '1' and cron will complain...
 if ! git status | grep -qs '^nothing to commit (working directory clean)$' ; then
-    # have git display the status if we're going to commit:
-    git status
-    echo '----------------------------------------------------------------'
-    # check diff size before committing, remember diff if small enough:
-    if [ $(git diff --cached | wc -l) -le 100 ] ; then
-        GITDIFF="$(git diff --cached)"
-    fi
     # '-a' is required to delete files in the repository that
     # have been deleted locally
-    git commit -a -m "git-etc autocommitting changes $(date +%F_%H-%M)"
-    # echo $?
-    if [ -n "${GITDIFF}" ] ; then
-        echo '----------------------------------------------------------------'
-        echo "${GITDIFF}"
+    COMMIT=$(git commit -a -m "git-autocommit: changes from $(date +%F_%H-%M)")
+    # get the hash-ID of our commit:
+    CID=$(git log -1 --pretty=format:%H)
+    # check diff size before committing, remember diff if small enough:
+    if [ $(git show --format=format:'' $CID | wc -l) -le 100 ] ; then
+        GITDIFF="$(git show --format=format:'' $CID)"
     fi
+    # print the header and the list of changed files (+modes +hashes):
+    git whatchanged -1
+    echo '-- SUMMARY -----------------------------------------------------'
+    # only print the summary line of the commit:
+    echo "$COMMIT" | sed -n 2p
     echo '----------------------------------------------------------------'
     # determine if we have a remote repository to push to:
     if git remote -v | grep -qs push ; then
         git push
     fi
+    echo "${GITDIFF}"
 fi
