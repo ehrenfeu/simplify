e107fe6a74d1560e0d5baded1f47a48e6b24c503
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2012-04-16 15:38:51 +0200

    only print selected chardata

--- misc/parse_excel_xml.py
+++ misc/parse_excel_xml.py
@@ -1,19 +1,31 @@
 import xml.parsers.expat
 
+print_chardata = False
+
 # define the handler functions
 def start_element(name, attrs):
-    # print 'start element: "',name, '", "', attrs, '"'
-    if name == 'ss:Worksheet':
-        print "worksheet, attrs:", attrs
+	global print_chardata
+	# print 'start element: "',name, '", "', attrs, '"'
+	if name == 'ss:Worksheet':
+		print "worksheet, attrs:", attrs
+		print_chardata = True
+
+def end_element(name):
+	global print_chardata
+	#print "end_element:", name
+	print_chardata = False
 
 def char_data(data):
-    print 'char data:', repr(data)
+	global print_chardata
+	if print_chardata:
+		print 'char data:', repr(data)
 
 # set the file to read
 infile = open('c:/Temp/green_single.xml')
 
 p = xml.parsers.expat.ParserCreate('UTF-8')
 p.StartElementHandler = start_element
-# p.CharacterDataHandler = char_data
+p.EndElementHandler = end_element
+p.CharacterDataHandler = char_data
 
 p.ParseFile(infile)
