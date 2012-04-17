99d128e92d34e0b65542052ce671b4fa0f99ab48
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2012-04-17 16:24:09 +0200

    remove testing code

--- misc/parse_excel_xml_etree.py
+++ misc/parse_excel_xml_etree.py
@@ -13,35 +13,6 @@ if not myns == expected_ns:
 	print "ERROR, this file doesn't have the expected XML namespace!"
 print "namespace parsed from XML document:", myns
 
-# etree.register_namespace('ss', 'urn:schemas-microsoft-com:office:spreadsheet')
-# for uri, prefix in etree._namespace_map.items():
-	# print "URI: ", uri, "  --  prefix:", prefix
-
-
-# root = tree.getroot()
-# print "tree.getroot():", root
-# print "root.tag:", root.tag
-# print
-
-# etree.dump(tree)
-
-#for child in root:
-#	print "child:", child
-#print
-
-worksheets = tree.findall('{%s}Worksheet' % myns)
-# print "worksheets:", worksheets
-# for ws in worksheets:
-	# print "---- worksheet attrib ----"
-	# print ws.attrib
-	# print "---- worksheet dump -----"
-	# etree.dump(ws)
-	# print
-
-
-# t = tree.findall('{%s}Worksheet/{%s}Table' % myns)
-# print t
-
 pattern = ".//{%s}Worksheet[@{%s}Name='Position']" % (myns, myns)
 
 ws_position = tree.findall(pattern)
@@ -56,6 +27,3 @@ for row in rows:
 	for att in row.keys():
 		print att + ' -> ' + row.get(att)
 	etree.dump(row)
-	# for child in row:
-		# print "child:", child
-
