20713706e4b7bec46cee31eb7d4c92ec6c46a6f2
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2012-04-17 17:06:12 +0200

    more cleanups

--- misc/parse_excel_xml_etree.py
+++ misc/parse_excel_xml_etree.py
@@ -4,17 +4,19 @@ import sys
 # for arg in sys.argv:
 	# print arg
 
-# FIXME: we need a way to set infile if we're not called via execfile()
-print "File to process: " + infile
+spots = []
 
+# FIXME: we need a way to set infile if we're not called via execfile()
+print "Processing file: " + infile
 tree = etree.parse(infile)
+print "Done parsing the XML."
 
 expected_ns = 'urn:schemas-microsoft-com:office:spreadsheet'
 myns = tree.getroot().tag[1:].split("}")[0]
 if not myns == expected_ns:
 	print "ERROR, this file doesn't have the expected XML namespace!"
 	sys.exit(1)
-print "namespace parsed from XML document:", myns
+print "Namespace parsed from XML document: '" + myns + "'"
 print
 
 # we're looking for stuff in the "Position" worksheet:
