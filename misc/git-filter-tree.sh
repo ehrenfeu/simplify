f454f158d4e6197463b6a55d3ac7da24a642b631
Niko Ehrenfeuchter <mail@he1ix.org>
2012-12-06 18:41:11 +0100

    add script to rewrite git history
    filters files from a git tree by moving them to a defined location
    and adding placeholders with information about what has been
    removed to the repo

---
+++ misc/git-filter-tree.sh
@@ -0,0 +1,52 @@
+# this echo is required since the "rewrite" message of git does
+# not have a trailing newline:
+echo
+
+REGEXPS="/home/ehrenfeu/usr/packages/simplify/misc/git-filter.regexps"
+rm /tmp/asdfasdf/COMMIT_EDITMSG_*
+
+# remember the existing log message (title + body):
+LOG_BODY=$(git log -1 --pretty=format:%s%n%n%b $GIT_COMMIT)
+echo "$LOG_BODY" > /tmp/asdfasdf/COMMIT_EDITMSG_2
+# echo "-- $LOG_BODY --"
+
+LOG_FULL=$(git log -1 --parents --name-status $GIT_COMMIT)
+
+MATCHES=$(git ls-files | egrep --only-matching -f $REGEXPS)
+for match in $MATCHES ; do
+    # checkout is not required in tree-filter:
+    ### git checkout $GIT_COMMIT $match
+
+    # create a placeholder file that goes into the repository
+    SHA1=$(sha1sum "$match" | cut -d ' ' -f 1)
+    MSG="
+    This is a placeholder for a file that was removed during repository
+    cleanup. This file changes exactly when the original file was renamed
+    or the file itself was changed.
+    original filename: '$match'
+    original sha1sum: $SHA1"
+    echo "$MSG" > "$match.orig-info"
+
+    # record the original log incl parent ids in a separate file
+    # NOTE: this file will change with EVERY commit where the original
+    # file was present, this is meant as an indicator for this!
+    echo "$LOG_FULL" > "$match.orig-log"
+
+    # remove the file from the repository:
+    TGT="/tmp/gittests/removed/$SHA1"
+    mkdir -p $TGT
+    mkdir -p $TGT/orig-commitlogs
+    mv -v $match $TGT
+    cp -v $match.orig-log $TGT/orig-commitlogs/$GIT_COMMIT
+
+    # add a note stating the removal of files to the log (ONE line!)
+    MSG_REMOVED="\n(NOTE: files were removed during cleanup)"
+    echo "$MSG_REMOVED" > /tmp/asdfasdf/COMMIT_EDITMSG_3
+
+    # adding the name of the removed file to the commit message really
+    # clutters up the log a lot, so we don't do this!
+    ### echo "removed file: $match" >> /tmp/asdfasdf/COMMIT_EDITMSG_4
+done
+echo '---'
+
+# git rm --cached --ignore-unmatch
