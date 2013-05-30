0b3fbae3e6364e91718cd735adbd0e272dc64299
Niko Ehrenfeuchter <mail@he1ix.org>
2013-05-30 16:31:27 +0200

    color abbreviations

---
+++ configs/bash_prompt_inc.sh
@@ -0,0 +1,14 @@
+red='\[\033[0;31m\]'   # red
+grn='\[\033[0;32m\]'   # green
+ylw='\[\033[0;33m\]'   # yellow
+blu='\[\033[0;34m\]'   # blue
+vio='\[\033[0;35m\]'   # violet
+mag='\[\033[0;36m\]'   # magenta
+bred='\[\033[01;31m\]' # bold red
+bgrn='\[\033[01;32m\]' # bold green
+bylw='\[\033[01;33m\]' # bold yellow
+bblu='\[\033[01;34m\]' # bold blue
+bvio='\[\033[01;35m\]' # bold violet
+bmag='\[\033[01;36m\]' # bold magenta
+
+host_ssh=$blw$red'@\h'
