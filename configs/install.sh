9c996a2a0ee880e08e8dba2491ef470a019285be
Niko Ehrenfeuchter <mail@he1ix.org>
2014-12-18 15:43:39 +0100

    Add simple installation script.

---
+++ configs/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $(pwd) != "$HOME" ] ; then
+    echo "Plese run this from your home dir!"
+    exit 1
+fi
+
+BASE=$(dirname $0)
+TSTAMP=$(date +%FT%H%M%S)
+
+install_link() {
+    if [ -s ".$1" ] ; then
+        mv .$1 .${1}.pre-$TSTAMP
+    fi
+    ln -sv "$BASE"/$1 .$1
+}
+
+CONFIGS="bashrc \
+bash_logout \
+bash_functions \
+profile"
+
+for file in $CONFIGS ; do
+    install_link $file
+done
+
+for file in "$BASE"/*.inc.sh ; do
+    install_link $(basename $file)
+done
