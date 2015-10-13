e1441ec2ee8a4d8e01d8d7827b8b7ca10172da5a
Niko Ehrenfeuchter <mail@he1ix.org>
2015-10-13 15:48:15 +0200

    Add sample script to compact VMDK disk images.

---
+++ misc/compact_vmdk.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# exit on any error:
+set -e
+
+VBOX_BASEDIR="$HOME/VirtualBox_VMs"
+cd "$VBOX_BASEDIR"
+
+VM_NAME="debian-7.8.0"
+cd "$VM_NAME"
+
+VMDK_NAME="$(ls *.vmdk -1 | head -n 1)"
+VMDK_FULL="$VBOX_BASEDIR/$VM_NAME/$VMDK_NAME"
+VDI_NAME="${VM_NAME}__VDI-TEMP.vdi"
+VDI_FULL="$VBOX_BASEDIR/$VM_NAME/$VDI_NAME"
+
+echo "reducing size of '$VMDK_FULL'"
+ls -hs "$VMDK_FULL"
+set -x
+
+vboxmanage showvminfo "$VM_NAME"
+vboxmanage storageattach "$VM_NAME" --storagectl SATA --port 0 --device 0 --type hdd --medium none
+vboxmanage clonehd --format vdi "$VMDK_FULL" "$VDI_FULL"
+# vboxmanage closemedium disk "$VMDK_FULL" --delete
+vboxmanage closemedium disk "$VMDK_FULL"
+mv -vi "$VMDK_FULL" "${VMDK_FULL}.orig"
+
+vboxmanage modifyhd "$VDI_FULL" --compact
+vboxmanage clonehd --format vmdk "$VDI_FULL" "$VMDK_FULL"
+# vboxmanage closemedium disk "$VDI_FULL" --delete
+vboxmanage closemedium disk "$VDI_FULL"
+
+vboxmanage storageattach "$VM_NAME" --storagectl SATA --port 0 --device 0 --type hdd --medium "$VMDK_FULL"
