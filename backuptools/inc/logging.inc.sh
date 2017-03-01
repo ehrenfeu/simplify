ab659fe5e361ab29afebf908f2b8b20f9ab05174
Niko Ehrenfeuchter <mail@he1ix.org>
2017-03-01 12:00:29 +0100

    Add loglevel prefixes to messages, add loge and logc.

--- backuptools/inc/logging.inc.sh
+++ backuptools/inc/logging.inc.sh
@@ -11,14 +11,22 @@ __LOG_LEVEL=30  # WARN
 [ "$LOG_VERBOSITY" = "INFO" ] && __LOG_LEVEL=20
 [ "$LOG_VERBOSITY" = "DEBUG" ] && __LOG_LEVEL=10
 
+logc() {
+    [ "$__LOG_LEVEL" -le 50 ] && echo "[CRITICAL] $*"
+}
+
+loge() {
+    [ "$__LOG_LEVEL" -le 40 ] && echo "[ERROR] $*"
+}
+
 logw() {
-    [ "$__LOG_LEVEL" -le 30 ] && echo "$*"
+    [ "$__LOG_LEVEL" -le 30 ] && echo "[WARN] $*"
 }
 
 logi() {
-    [ "$__LOG_LEVEL" -le 20 ] && echo "$*"
+    [ "$__LOG_LEVEL" -le 20 ] && echo "[INFO] $*"
 }
 
 logd() {
-    [ "$__LOG_LEVEL" -le 10 ] && echo "$*"
+    [ "$__LOG_LEVEL" -le 10 ] && echo "[DEBUG] $*"
 }
