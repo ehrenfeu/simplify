416614ca9b75e640664dcb0f704c4ed7fb02435b
Niko Ehrenfeuchter <mail@he1ix.org>
2017-03-02 18:37:36 +0100

    Add functions to check return values and compress files.
    Plus printing of the corresponding messages.

--- backuptools/inc/misc.inc.sh
+++ backuptools/inc/misc.inc.sh
@@ -16,3 +16,31 @@ locate_config_file() {
     fi
 }
 
+
+check_return_value() {
+    # check if the supplied value is '0' (success) or exit otherwise, printing
+    # the corresponding success or error messages
+    RET=$1
+    if [ "$RET" -eq "0" ] ; then
+        _pb "  --> SUCCESS!"
+    else
+        _pb
+        _pb "       ****** ************* ******"
+        _pb "  -->  ****** !!! ERROR !!! ******"
+        _pb "       ****** ************* ******"
+        _pb_footer
+        exit 2
+    fi
+}
+
+
+compress_backup_file() {
+    if [ -z "$_compress" ] ; then
+        logd "No compression tool configured, doing nothing."
+        return
+    fi
+    _pb
+    _pb "compressing file:"
+    _compress "$1"
+    _pb_file_with_size "${1}.$_compress_suffix"
+}
