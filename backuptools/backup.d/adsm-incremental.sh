9020f30f5954b0bf12c29f37a6c94a75144db729
Niko Ehrenfeuchter <mail@he1ix.org>
2017-03-02 18:08:44 +0100

    Add ADSM / TSM backup script + config.

---
+++ backuptools/backup.d/adsm-incremental.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+
+[ -z "$PP_BOX_WIDTH" ] && PP_BOX_WIDTH="76"
+LOG_VERBOSITY="WARN"
+
+set -e
+
+# source the common functions
+source "${HOME}/.backuptools/functions.inc.sh"
+
+logd "This is '$0'"
+
+locate_config_file
+
+# # # # # # # #
+
+tsm_client_incr() {
+    for i in $(seq 1 10) ; do
+        logi "calling '$DSMC incr \"$*\"'"
+        $DSMC incr "$*"
+    	STATUS="$?"
+    	if test "$STATUS" != "0" ; then
+    		loge "TSM-client returned status '$STATUS'"
+    		loge "--> sleeping for 5 minutes, then trying again ($i/10)"
+    	else
+            logi "TSM client successfully finished (in attempt $i/10)"
+    		break
+    	fi
+    	sleep 300
+    done
+}
+
+# # # # # # # #
+
+_file_executable_or_exit "$DSMC"
+_check_target_path "${STORE}"
+
+echo
+_pb_title
+_pb
+
+if ! test "$(id -u)" == "0" ; then
+    loge 'root privileges required, stopping!'
+    exit 1
+fi
+
+### ! ! ! WARNING ! ! ! ###
+# paths have to be specified *with* a trailing slash!!!
+# otherwise the tsm-call takes ages and even misses new files!
+tsm_client_incr "${STORE}/" | _pb_stdin
+
+_pb_footer
