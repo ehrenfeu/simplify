d62eaffead2cb1024877c7f598c0e9bc9b743c50
Niko Ehrenfeuchter <mail@he1ix.org>
2017-03-02 11:55:58 +0100

    Add the central backup control script.

---
+++ backuptools/run-backup-control.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+
+set -e
+
+# we expect a folder ".backuptools" in the home directory containing the
+# configs, the function definitions and the backup plugin scripts:
+CONF="${HOME}/.backuptools/configs/COMMON"
+if ! [ -r "$CONF" ] ; then
+    echo "ERROR: can't find backuptools config file '$CONF'!"
+    exit 255
+fi
+source "$CONF"
+
+# source the common functions
+source "${HOME}/.backuptools/functions.inc.sh"
+export BACKUPTOOLS_INC  # required for the plugin scripts below
+
+
+### <sanity>
+_check_hostname
+_check_target_path "${BAKDIR}"
+### </sanity>
+
+## run all scripts in the given directory
+## NOTE: *not* using run-parts is done by intention since
+##       they are incompatible between Debian <-> RHEL
+for file in $(ls ${BACKUP_D}) ; do
+	if _file_executable "${BACKUP_D}/$file" ; then
+		${BACKUP_D}/$file "${BAKDIR}"
+	fi
+done
