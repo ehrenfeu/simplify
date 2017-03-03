84f01c1f5b61a696510776ba34cc70c720afbee0
Niko Ehrenfeuchter <mail@he1ix.org>
2017-03-03 11:47:42 +0100

    Add rsync backup script + configuration.

---
+++ backuptools/backup.d/rsync.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+[ -z "$PP_BOX_WIDTH" ] && PP_BOX_WIDTH="76"
+LOG_VERBOSITY="WARN"
+
+_rsync="/usr/bin/rsync"
+_timeout="/usr/bin/timeout"
+
+set -e
+
+# source the common functions
+source "${HOME}/.backuptools/functions.inc.sh"
+
+_usage() {
+	echo
+	echo "USAGE: $0"
+	echo
+}
+
+logd "This is '$0'"
+
+locate_config_file
+
+echo
+_pb_title "rsync ($BAKSRC)"
+
+_file_executable_or_exit "$_timeout"
+_file_executable_or_exit "$_rsync"
+
+############################
+
+if ! [[ -d "$BAKSRC" || -d "$BAKDST" || -d "$RSYNC_OPTS" || -d "$LOGDST" ]] ; then
+	echo "$0 error in config file, check specified options!"
+	exit 1
+fi
+
+_check_target_path "${BAKDST}"
+
+if [ -z "$LOGNAME" ] ; then
+    LOGNAME="rsync"
+fi
+LOGTGT="${LOGDST}/${LOGNAME}-${DATEymd}.log"
+
+logi "$_rsync $RSYNC_OPTS $BAKSRC $BAKDST"
+logi
+
+echo "-------------------------" >> "$LOGTGT"
+date >> "$LOGTGT"
+
+$_rsync $RSYNC_OPTS "$BAKSRC" "$BAKDST" >> "$LOGTGT"
+
+date >> "$LOGTGT"
+
+cat "$LOGTGT" | _pb_stdin
+_pb_footer
