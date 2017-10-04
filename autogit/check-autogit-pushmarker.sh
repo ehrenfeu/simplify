f33c9034225ddf826cad19418167eee83d7f5320
Niko Ehrenfeuchter <mail@he1ix.org>
2017-10-04 09:33:30 +0200

    Set DISPLAY variable for notify-send.

--- autogit/check-autogit-pushmarker.sh
+++ autogit/check-autogit-pushmarker.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 #
+# This script is intended to be run e.g. from a user's crontab to check if a
+# marker file from the "autogit-commit" script exists that denotes a failed
+# push. In case the marker is there, a message is sent to the desktop using
+# the "notify-send" tool.
 
 # set -x
+
+# setting DISPLAY is required to make "notify-send" work from the crontab as
+# it needs to figure out the dbus session address (can be overridden from the
+# crontab entry if necessary)::
+[ -z "$DISPLAY" ] && export DISPLAY=:0
 
 MARKER="/tmp/autogit_push_failed"
 
