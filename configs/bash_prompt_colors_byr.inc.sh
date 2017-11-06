60526da630ddff3349b9d79a72da884880753fe3
Niko Ehrenfeuchter <mail@he1ix.org>
2017-11-06 11:30:18 +0100

    Add blue-yellow-red prompt color scheme.

---
+++ configs/bash_prompt_colors_byr.inc.sh
@@ -0,0 +1,9 @@
+# prompt color profile:
+#   username - bright blue
+#   @ - yellow
+#   host - red, in yellow brackets
+#
+# to use this profile symlink it to ~/.bash_prompt_colors.inc.sh
+
+host=$blw$bylw'@['$bred'\h'$bylw']'$blw
+usrclr=$blw$bblu
