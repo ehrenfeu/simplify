db7a1513016922b0c3c0f64942c719d584b83719
Niko Ehrenfeuchter <mail@he1ix.org>
2017-11-10 14:14:38 +0100

    Add explanation why "tput" is not being used, add cyan.

--- configs/bash_prompt.inc.sh
+++ configs/bash_prompt.inc.sh
@@ -1,22 +1,30 @@
-# TODO: use `tput` for generating the escape codes below (see
-# https://askubuntu.com/questions/24358 for details)
+# NOTE: using `tput` for generating the escape codes as described in [1]
+# doesn't work on the console, plus it messes up line length calculations for
+# bash, breaking (at least) the gnome-terminal's behaviour on line continuation
+# / newlines / resize
+# [1]: https://askubuntu.com/questions/24358 for details)
+
 # color shorthands
 blw='\[\033[00m\]'     # black-white
+
 red='\[\033[0;31m\]'   # red
 grn='\[\033[0;32m\]'   # green
 ylw='\[\033[0;33m\]'   # yellow
 blu='\[\033[0;34m\]'   # blue
 vio='\[\033[0;35m\]'   # violet
-mag='\[\033[0;36m\]'   # magenta
+mag="$vio"             # magenta
+cyn='\[\033[0;36m\]'   # cyan
+
 bred='\[\033[01;31m\]' # bold red
 bgrn='\[\033[01;32m\]' # bold green
 bylw='\[\033[01;33m\]' # bold yellow
 bblu='\[\033[01;34m\]' # bold blue
 bvio='\[\033[01;35m\]' # bold violet
-bmag='\[\033[01;36m\]' # bold magenta
+bmag="$bvio"           # bold magenta
+bcyn='\[\033[01;36m\]' # bold cyan
 
 # set the default color for the username:
-usrclr=$grn
+usrclr=$cyn
 
 # set a prefix if we're inside a screen since old screen-versions (or
 # non-ubuntu ones) can't be easily distinguished from a regular session
