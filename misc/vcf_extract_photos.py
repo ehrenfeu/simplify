b513ee3d1b9d99febc8652669fb50c33fee2def1
Niko Ehrenfeuchter <mail@he1ix.org>
2018-08-19 00:12:52 +0200

    Add basic script to extract photos from VCF

---
+++ misc/vcf_extract_photos.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""Extract photos from all .VCF files in a given directory."""
+
+import sys
+import argparse
+import os.path
+import vobject
+
+
+def process_vcf_file(pathname):
+    """Process a VCF file (or all in a directory)."""
+    outfname = os.path.splitext(pathname)[0]
+    with open(pathname, 'r') as vcf_file:
+        vcf_string = vcf_file.read()
+        vcard = vobject.readOne(vcf_string)
+        photo_type = vcard.photo.params['TYPE'][0].lower()
+        outfname += "." + photo_type
+        print("Processing '%s'..." % vcard.fn.value)
+        print("  - UID: %s" % vcard.uid.value)
+        print("  - type: %s" % photo_type)
+        if os.path.exists(outfname):
+            print("NOT overwriting file '%s'!" % outfname)
+            return
+        print("  - writing photo to file: %s" % outfname)
+        with open(outfname, 'wb') as fout:
+            fout.write(vcard.photo.value)
+        print("  - DONE!\n")
+
+
+def parse_arguments():
+    """Parse commandline arguments."""
+    argparser = argparse.ArgumentParser(description=__doc__)
+    add = argparser.add_argument
+    add('--vcf', required=True,
+        help='VCF file to extract photo from.')
+    add('-v', '--verbosity', dest='verbosity',
+        action='count', default=0)
+    try:
+        args = argparser.parse_args()
+    except IOError as err:
+        argparser.error(str(err))
+    return args
+
+
+def main():
+    """Parse commandline arguments and run parser."""
+    args = parse_arguments()
+    process_vcf_file(args.vcf)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
